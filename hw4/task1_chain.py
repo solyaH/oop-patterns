0c57c8bc675065574ed58e8f9a598f3ba331a298
solyaH <hatalyak.s@gmail.com>
2019-07-05 18:34:15 +0300

    Little fixes

--- hw4/task1_chain.py
+++ hw4/task1_chain.py
@@ -1,4 +1,4 @@
-class SomeObject:
+class Someobjcect:
     def __init__(self):
         self.integer_field = 0
         self.float_field = 0.0
@@ -19,66 +19,66 @@ class NullHandler:
     def __init__(self, successor=None):
         self. __successor = successor
 
-    def handle(self, obj, event):
+    def handle(self, objc, event):
         if self. __successor is not None:
-            return self. __successor.handle(obj, event)
+            return self. __successor.handle(objc, event)
 
 
 class IntHandler(NullHandler):
-    def handle(self, obj, event):
+    def handle(self, objc, event):
         if isinstance(event, EventGet):
             if event.type_ is int:
-                return obj.integer_field
+                return objc.integer_field
             else:
-                return super().handle(obj, event)
+                return super().handle(objc, event)
         elif isinstance(event, EventSet):
             if isinstance(event.value_, int):
-                obj.integer_field = event.value_
+                objc.integer_field = event.value_
             else:
-                return super().handle(obj, event)
+                super().handle(objc, event)
 
 
 class FloatHandler(NullHandler):
-    def handle(self, obj, event):
+    def handle(self, objc, event):
         if isinstance(event, EventGet):
             if event.type_ is float:
-                return obj.float_field
+                return objc.float_field
             else:
-                return super().handle(obj, event)
+                return super().handle(objc, event)
         elif isinstance(event, EventSet):
             if isinstance(event.value_, float):
-                obj.float_field = event.value_
+                objc.float_field = event.value_
             else:
-                return super().handle(obj, event)
+                super().handle(objc, event)
 
 
 class StrHandler(NullHandler):
-    def handle(self, obj, event):
+    def handle(self, objc, event):
         if isinstance(event, EventGet):
             if event.type_ is str:
-                return obj.string_field
+                return objc.string_field
             else:
-                return super().handle(obj, event)
+                return super().handle(objc, event)
         elif isinstance(event, EventSet):
             if isinstance(event.value_, str):
-                obj.string_field = event.value_
+                objc.string_field = event.value_
             else:
-                return super().handle(obj, event)
+                super().handle(objc, event)
 
 
 if __name__ == '__main__':
-    obj = SomeObject()
-    obj.integer_field = 42
-    obj.float_field = 3.14
-    obj.string_field = 'some text'
+    objc = Someobjcect()
+    objc.integer_field = 42
+    objc.float_field = 3.14
+    objc.string_field = 'some text'
 
     chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
-    print(chain.handle(obj, EventGet(int)))
-    print(chain.handle(obj, EventGet(float)))
-    print(chain.handle(obj, EventGet(str)))
-    print(chain.handle(obj, EventSet(100)))
-    print(chain.handle(obj, EventGet(int)))
-    print(chain.handle(obj, EventSet(0.5)))
-    print(chain.handle(obj, EventGet(float)))
-    print(chain.handle(obj, EventSet('new text')))
-    print(chain.handle(obj, EventGet(str)))
+    print(chain.handle(objc, EventGet(int)))
+    print(chain.handle(objc, EventGet(float)))
+    print(chain.handle(objc, EventGet(str)))
+    chain.handle(objc, EventSet(100))
+    print(chain.handle(objc, EventGet(int)))
+    chain.handle(objc, EventSet(0.5))
+    print(chain.handle(objc, EventGet(float)))
+    chain.handle(objc, EventSet('new text'))
+    print(chain.handle(objc, EventGet(str)))
